db15b53fb0a93b778387e24bd91cc42ef224f757
ben smith <benjaminmith1981@gmail.com>
2017-06-14 16:24:28 +0200

    New project added to start!!

---
+++ DiveAdvisor/DiveAdvisor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiveAdvisor
+//
+//  Created by ben smith on 14/06/17.
+//  Copyright © 2017 ben smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
