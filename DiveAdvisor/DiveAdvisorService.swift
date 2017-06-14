47bd9bec4f5911ec7da8e0379ed9cf4fcd1cf5bc
Achid Farooq <achidfarooq@gmail.com>
2017-06-14 17:53:19 +0200

    Started adding alamofire to project + data layer beginning

---
+++ DiveAdvisor/DiveAdvisorService.swift
@@ -0,0 +1,11 @@
+//
+//  DiveAdvisorService.swift
+//  DiveAdvisor
+//
+//  Created by Achid Farooq on 14-06-17.
+//  Copyright © 2017 ben smith. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
---
+++ DiveAdvisor/Pods/Target Support Files/Pods-DiveAdvisor/Pods-DiveAdvisor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiveAdvisor : NSObject
+@end
+@implementation PodsDummy_Pods_DiveAdvisor
+@end
