d1b015b8576ab5c82b1aeafc9f9691018e96cce2
Paul Geurts <pmgeurts@gmail.com>
2017-06-15 22:44:43 +0200

    - Implemented search controller table view - Implemented map view - Implemented segue to detail view

--- DiveAdvisor/DiveAdvisor/ViewController.swift
+++ DiveAdvisor/DiveAdvisor/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import MapKit
+
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
 
 class ViewController: UIViewController {
-
+    
+    let locationManager = CLLocationManager()
+    var resultSearchController: UISearchController? = nil
+    var selectedPin:MKPlacemark? = nil
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: viewControllerIDs.locationSearchTable) as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +51,97 @@ class ViewController: UIViewController {
     }
 
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MapToDetail" {
+//            let currentAnnotation = mapView.selectedAnnotations[0] //as! FestivalMapAnnotation
+//            let destination = segue.destination as? UITableViewController
+//            destination?.theFestival = currentAnnotation.festival
+        }
+    }
+
+}
+
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+
+    
+    // MAYBE DELETE PART ABOUT THE FIRST ONE, WE WANT ALL SITES I GUESS - PG
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print(location)
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: (error)")
+    }
+
+}
+
+extension ViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "(city) (state)"
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension ViewController : MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        performSegue(withIdentifier: "MapToDetail", sender: self)
+        // spot to display more detailed information for dive sites
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        
+        let identifier = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        pinView?.pinTintColor = UIColor.blue
+        pinView?.canShowCallout = true
+        pinView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+        
+        /* Code to use when we would like to implement a custom icon to segue to detail
+         let identifier = "pin"
+         var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+         pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+         pinView?.pinTintColor = UIColor.blue
+         pinView?.canShowCallout = true
+         let smallSquare = CGSize(width: 30, height: 30)
+         let button = UIButton(frame: CGRect(origin: CGPointZero, size: smallSquare))
+         button.setBackgroundImage(UIImage(named: "car"), forState: .Normal)
+         button.addTarget(self, action: "getDirections", forControlEvents: .TouchUpInside)
+         pinView?.leftCalloutAccessoryView = button
+         return pinView
+         */
+        
+        
+        return pinView
+    }
+    
 }
 
